782be4371f475e1cf4031b95d3cb58909250ed14
Ankidom <35579006+Ankidom@users.noreply.github.com>
2023-05-29 17:56:16 +0200

    Register pagina en database gemaakt
    Moet alleen nog maar de depandancy verwijderen die niet nodig zijn.

--- Factorio Blueprint Hub/Backend/server.js
+++ Factorio Blueprint Hub/Backend/server.js
@@ -1,48 +1,48 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
 
 // Initialize SQLite database
-let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database('./db.sqlite', (err) => {
     if (err) {
         return console.error(err.message);
     }
-    console.log('Connected to the in-memory SQLite database.');
+    console.log('Connected to the SQLite database.');
 });
 
 // Create Users table
-db.run('CREATE TABLE Users(username TEXT PRIMARY KEY, password TEXT NOT NULL)', [], (err) => {
-    if (err) {
-        console.error(err.message);
-    }
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS Users (ID INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL, Email TEXT NOT NULL UNIQUE, Password TEXT NOT NULL, Created_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP)");
 });
 
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    db.run(`INSERT INTO Users(username, password) VALUES(?, ?)`, [username, password], function (err) {
+
+
+app.post('/register', function(req, res) {
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    // Sla de gebruiker op in de database
+    db.run("INSERT INTO Users (username, email, password) VALUES (?, ?, ?)", [username, email, password], function(err) {
         if (err) {
-            return res.status(400).send({ error: err.message });
+            // Stuur een error bericht als er iets misgaat
+            res.status(500).send("Error registering user");
+        } else {
+            // Stuur een succesbericht als het goed gaat
+            res.send("User registered successfully");
         }
-        res.status(201).send({ message: 'User registered successfully' });
     });
 });
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    db.get('SELECT * FROM Users WHERE username = ?', [username], (err, row) => {
-        if (err) {
-            return res.status(400).send({ error: err.message });
-        }
-        if (!row || row.password !== password) {
-            return res.status(400).send({ error: 'Login failed! Check your credentials' });
-        }
-        const token = jwt.sign({ username: row.username }, 'secretkey', { expiresIn: '1h' });
-        res.send({ message: 'User logged in successfully', token });
-    });
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
